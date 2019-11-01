daacc41f74a0734090ce407ff3ab5261f447d72e
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-01 10:32:02 -0500

    imported axios; making get request; passing data as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PlayerCard from './components/PlayerCard';
+
+class App extends Component {
+  state = {
+    playerData: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        // console.log(res.data)
+        this.setState({
+          playerData: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Advanced React: Women's World Cup</h1>
+        {this.state.playerData.map((player, id) => {
+          return (
+            <PlayerCard player={player} key={player.id}/>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/PlayerCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayerCard = props => {
+    console.log('PlayerCard Props: ', props)
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PlayerCard;
