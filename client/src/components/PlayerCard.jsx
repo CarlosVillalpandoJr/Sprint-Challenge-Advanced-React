ddfa892d2679d13d4dd4664a288f9d76433a041e
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-01 10:34:15 -0500

    displaying player data

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const PlayerCard = props => {
     console.log('PlayerCard Props: ', props)
     return (
-        <div>
-
+        <div className='player-card'>
+            <h3>{props.player.name} | {props.player.country}</h3>
+            <p>Number of searches: {props.player.searches}</p>
         </div>
     )
 }
